6d51f29a9b06ed04f039e6babc80586b239cb4de
victor <victorugwueze@gmail.com>
2020-01-07 00:32:56 +0100

    create and get all books

--- app.py
+++ app.py
@@ -14,3 +14,30 @@ from models import Book
 @app.route("/")
 def hello():
   return "Hello world"
+
+
+
+@app.route("/books/create", methods=["POST"])
+def create_book():
+  data = request.get_json()
+  name = data['name']
+  author = data['author']
+  published = data['published']
+  try:
+    book = Book(name=name, author=author, published=published)
+    db.session.add(book)
+    db.session.commit()
+    return "Book name={} added successfully".format(book.name)
+  except Exception as e:
+    return (str(e))
+
+@app.route("/books")
+def get_all():
+  try:
+    books = Book.query.all()
+    return jsonify([e.serialize() for e in books])
+  except Exception as e:
+    return (str(e))
+
+if __name__ == '__main__':
+  app.run('0.0.0.0')
